59aaa5702b7b8f8f5bcbdfd41c1a795fafafe145
Serg <serggbobrov@gmail.com>
2020-08-19 10:52:58 +0300

    first commint

---
+++ it-react-1/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Acrordion/Acordion";
+import {Rating} from "./components/Rating/Rating";
+
+function App() {
+    console.log("App rendered")
+
+    return (
+        <div className="App">
+            <PageTitle title={"Tish is APP component"}/>
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"}/>
+            <Accordion titleValue={"Users"}/>
+            <Rating value={4}/>
+        </div>
+    );
+}
+
+
+function hello() {
+    debugger
+    alert("hello");
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return <>{props.title} </>
+}
+
+export default App;
