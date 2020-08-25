1342827a1c9155e012d819cd51c2d090dbd9818a
Serg <serggbobrov@gmail.com>
2020-08-25 16:59:18 +0300

    first commint

---
+++ src/components/Acrordion/Acordion.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue : string
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log("Accordion rendered")
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} />
+            <AccordionBody/>
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+
+export default Accordion;
