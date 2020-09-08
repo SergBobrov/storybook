ac422b3d2f20abc08e06c2221f5fdd3c91dd0cb2
SergBobrov <serggbobrov@gmail.com>
2020-09-08 19:12:37 +0300

    first commint

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,41 +1,71 @@
 import React, {useState} from "react";
 
 
-type RatingPropsValue = {
+type RatingPropsValue = {};
 
-};
-
+type ValueType = {
+    value: number
+    selected: boolean
+}
 
 export function UncontrolledRating(props: RatingPropsValue) {
-    console.log("Accordion rendered")
+
 
     let [value, setValue] = useState(0);
 
-        return (
-            <div>
-                <Star selected={value>0}/> <button onClick={()=> setValue(1)}>1</button>
-                <Star selected={value>1}/> <button onClick={()=> setValue(2)}>2</button>
-                <Star selected={value>2}/> <button onClick={()=> setValue(3)}>3</button>
-                <Star selected={value>3}/> <button onClick={()=> setValue(4)}>4</button>
-                <Star selected={value>4}/> <button onClick={()=> setValue(5)}>5</button>
-            </div>
+    let [stars, setStars] = useState<Array<ValueType>>([
+        {value: 1, selected: false},
+        {value: 2, selected: false},
+        {value: 3, selected: false},
+        {value: 4, selected: false},
+        {value: 5, selected: false}
+    ])
+
+    function activateStar(value: number) {
+        let newStars = stars.map((t) => {
+                if (value === t.value) {
+                    t.selected = !t.selected
+                }
+                return t
+            }
         )
+        setStars([...newStars])
+        console.log(stars);
+
+
     }
 
-
+    return (
+        <div>
+            {stars.map((t) => {
+                return (
+                    <Star stars={t} activateStar={activateStar}/>
+                )
+            })}
+        </div>
+    )
+    /* return (
+         <div>
+             <Star selected={value>0}/> <button onClick={()=> setValue(1)}>1</button>
+             <Star selected={value>1}/> <button onClick={()=> setValue(2)}>2</button>
+             <Star selected={value>2}/> <button onClick={()=> setValue(3)}>3</button>
+             <Star selected={value>3}/> <button onClick={()=> setValue(4)}>4</button>
+             <Star selected={value>4}/> <button onClick={()=> setValue(5)}>5</button>
+         </div>
+     )*/
+}
 
 
 type StarPropsType = {
-    selected : boolean
+    stars: ValueType
+    activateStar: (t: number) => void
 }
 
 function Star(props: StarPropsType) {
-    if (props.selected) {
-        return (
-            <span><b>Star </b></span>
-        )
-    } else
-        return (
-            <span>Star </span>
-        )
+
+    return (
+        <span onClick={() => props.activateStar(props.stars.value)}> {props.stars.selected ?
+            <b>Star </b> : "Star"}</span>
+    )
+
 }
