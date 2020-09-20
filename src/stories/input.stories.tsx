01f039002923929936ee92fc540c4c1d792d7bd9
Serg <serggbobrov@gmail.com>
2020-09-20 15:51:26 +0300

    story book

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -59,6 +60,46 @@ export const GetValueOfUncontrolledInputByButtonPressAlternative = () => {
             - {value} </>
     );
 }
+
+export const ControlledInput = () => {
+
+    let [parentValue, setParentValue] = useState("")
+    debugger
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+
+    return (
+        <input value={parentValue} onChange={onChange}/>
+    )
+};
+
+export const ControlledCheckbox = () => {
+    let [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.checked)
+
+    return (
+        <input type="checkbox" checked={parentValue} onChange={onChange}/>
+    )
+
+};
+
+export const ControlledSelect = () => {
+    let [parentValue, setParentValue] = useState("")
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => setParentValue(e.currentTarget.value)
+
+    return (
+        <select value={parentValue} onChange={onChange}>
+            <option value={"1"}>"Minsk"</option>
+            <option value={"2"}>"Moskow"</option>
+            <option value={"3"}>"Kiev"</option>
+
+
+        </select>
+    )
+
+};
 
 
 export const InputWithFixedValue = () => <input value={"it-incubator.by"}/>;
